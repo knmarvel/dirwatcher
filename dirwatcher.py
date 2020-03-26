75d0c52493c80d452720c042d7f3426a550af72e
Karen Marvel <knmarvel@gmail.com>
2020-03-26 15:31:12 -0400

    dirwatcher returns true if directory contains specified filetype with magic string

--- dirwatcher.py
+++ dirwatcher.py
@@ -7,10 +7,26 @@ __author__ = "knmarvel"
 
 import argparse
 import sys
+import os
 
 
-def dirwatcher():
+def check_for_string(magic, search_dir, file_type):
+    files_with_magic = []
+    if os.path.exists(search_dir):
+        for root, dirs, filenames in os.walk(search_dir):
+            for filename in filenames:
+                if file_type in filename:
+                    with open(filename, "r") as file:
+                        file = file.read().splitlines()
+                        for line in file:
+                            if magic in line:
+                                files_with_magic.append(
+                                    [filename, file.index(line)])
+    return len(files_with_magic) > 0
 
+
+def dirwatcher(interval, magic, search_dir, file_type):
+    return check_for_string(magic, search_dir, file_type)
 
 
 def create_parser(*args, **kwargs):
@@ -30,7 +46,9 @@ def create_parser(*args, **kwargs):
 
 def main():
     args = create_parser()
-    
+    answer = dirwatcher(args.interval, args.magic, args.d, args.t)
+    print(answer)
+    return answer
 
 
 if __name__ == "__main__":
--- tests/test_dirwatcher.py
+++ tests/test_dirwatcher.py
@@ -8,10 +8,11 @@ class TestDirwatcher(unittest.TestCase):
     def test_searchstring(self):
         """Tests if the search string is found in the given directory"""
         process = subprocess.Popen(
-            ["python", "./dirwatcher.py", "1", "Hello", "-d .", "-t txt"],
+            ["python", "./dirwatcher.py", "1", "Hello", "-d", ".", "-t", "txt"],
             stdout=subprocess.PIPE)
-        stdout, _ = process.communicate().decode("utf-8")
-        self.assertTrue(stdout)
+        stdout, _ = process.communicate()
+        stdout = stdout.decode("utf-8")
+        self.assertEqual(stdout, 'True\n')
 
 
 if __name__ == '__main__':
